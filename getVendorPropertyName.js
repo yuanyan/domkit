aca15b13338c42e1bf844780d410e0d39382d746
元彦 <yuanyan.cao@alibaba-inc.com>
2015-10-20 00:21:11 +0800

    Flex about

--- getVendorPropertyName.js
+++ getVendorPropertyName.js
@@ -4,32 +4,56 @@ var builtinStyle = document.createElement('div').style;
 var prefixes = ['Moz', 'Webkit', 'O', 'ms'];
 var domVendorPrefix;
 
+// 2009 spec only
+var flexbox = {
+  flex: ['WebkitFlex', 'WebkitBoxFlex'],
+  order: ['WebkitOrder','WebkitBoxOrdinalGroup'],
+  // https://github.com/postcss/autoprefixer/blob/master/lib/hacks/flex-direction.coffee
+  flexDirection: ['WebkitFlexDirection', 'WebkitBoxOrient', 'WebkitBoxDirection'],
+  // https://github.com/postcss/autoprefixer/blob/master/lib/hacks/align-items.coffee
+  alignItems: ['WebkitAlignItems', 'WebkitBoxAlign'],
+  // https://github.com/postcss/autoprefixer/blob/master/lib/hacks/justify-content.coffee
+  justifyContent: ['WebkitJustifyContent', 'WebkitBoxPack'],
+  flexWrap: ['WebkitFlexWrap'],
+  alignSelf: ['WebkitAlignSelf'],
+}
+
 // Helper function to get the proper vendor property name. (transition => WebkitTransition)
 module.exports = function(prop) {
+
   var vendorProp;
   if (prop in builtinStyle) return prop;
-  var UpperProp = prop.charAt(0).toUpperCase() + prop.substr(1);
 
-  if (domVendorPrefix) {
-    vendorProp = domVendorPrefix + UpperProp;
-    if (vendorProp in builtinStyle) {
-      return vendorProp;
-    }else{
-      return prop;
-    }
-
-  } else {
-
-    for (var i = 0; i < prefixes.length; ++i) {
-      vendorProp = prefixes[i] + UpperProp;
-      if (vendorProp in builtinStyle) {
-        domVendorPrefix = prefixes[i];
-        return vendorProp;
+  if(flexbox[porp]){
+    // TODO: cache the result
+    var flexProperties = flexbox[porp];
+    for (var i = 0; i < flexProperties.length; ++i) {
+      if (flexProperties[i] in builtinStyle) {
+        return flexProperties[i];
       }
     }
 
-    if(!domVendorPrefix) {
-      return prop;
+  }else{
+
+    var UpperProp = prop.charAt(0).toUpperCase() + prop.substr(1);
+
+    if (domVendorPrefix) {
+
+      vendorProp = domVendorPrefix + UpperProp;
+      if (vendorProp in builtinStyle) {
+        return vendorProp;
+      }
+    } else {
+
+      for (var i = 0; i < prefixes.length; ++i) {
+        vendorProp = prefixes[i] + UpperProp;
+        if (vendorProp in builtinStyle) {
+          domVendorPrefix = prefixes[i];
+          return vendorProp;
+        }
+      }
     }
   }
+
+  return prop;
 }
